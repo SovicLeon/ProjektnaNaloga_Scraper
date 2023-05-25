70f9f7ec3f1c73bc61b68be892320bc0805ea4b6
SovicLeon <sovicleon@gmail.com>
2023-05-25 20:19:51 +0200

    Fixed script, coords displayed better

--- editCoordsTXT.py
+++ editCoordsTXT.py
@@ -8,10 +8,20 @@ for line in lines:
     match = re.search('\[.*\];?', line)  # Find everything between first [ and last ], including following ;
     if match:
         coords = match.group(0).rstrip('; ')  # Remove trailing semicolon and whitespace
-        info_parts = re.split('\[.*\];?', line)  # Split the line into two parts
-        # Rearrange the parts so that the coordinates are at the end
-        new_line = info_parts[0].rstrip() + ';' + info_parts[1].strip() + ';' + coords
-        new_lines.append(new_line + '\n')
+        # If 'Lat=' and 'Lon=' are present in the coords string
+        if 'Lat=' in coords and 'Lon=' in coords:
+            info_parts = re.split('\[.*\];?', line)  # Split the line into two parts
+
+            # Extract the lat and lon values
+            lat_match = re.search('Lat=([-\d.]+)', coords)
+            lon_match = re.search('Lon=([-\d.]+)', coords)
+
+            lat = lat_match.group(1) if lat_match else ''
+            lon = lon_match.group(1) if lon_match else ''
+
+            # Rearrange the parts so that the latitude and longitude are at the end
+            new_line = info_parts[0].rstrip() + ';' + info_parts[1].strip() + ';' + lat + ';' + lon
+            new_lines.append(new_line + '\n')
 
 with open('coords_new.txt', 'w', encoding='utf-8') as f:
     f.writelines(new_lines)
